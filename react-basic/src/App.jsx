c63cc6170d79521ff80664f4a7408490fcd89b3a
goldstar <rootsik1221@gmail.com>
2025-06-17 17:47:49 +0900

    react basic event handling, props

--- react-basic/src/App.jsx
+++ react-basic/src/App.jsx
@@ -2,14 +2,29 @@ import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import Button from "./components/Button";
 
 function App() {
   // Parent Component
+  const buttonProps = {
+    text: "mail",
+    color: "red",
+    a: 1,
+    b: 2,
+    c: 3,
+  };
+
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      {/* <Button text={"mail"} color={"red"} /> */}
+      <Button {...buttonProps} />
+      <Button text={"cafe"} />
+      <Button text={"blog"}>
+        <div> 자식요소 </div>
+      </Button>
+      <Button text={"test"}>
+        <Header />
+      </Button>
     </>
   );
 }
